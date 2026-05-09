528c26bed4b744dfa410c537c749ae5d4477e913
nazwaacamila6 <meelahtrash@gmail.com>
2026-05-09 10:59:35 +0700

    CreateEventScreen.kt

--- app/src/main/java/com/example/bazent/ui/screen/CreateEventScreen.kt
+++ app/src/main/java/com/example/bazent/ui/screen/CreateEventScreen.kt
@@ -1,2 +1,29 @@
 package com.example.bazent.ui.screen
 
+import androidx.compose.foundation.*
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.LocationOn
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import com.example.bazent.R
+import com.example.bazent.ui.theme.* // Pastikan warna SoftBlue, PrimaryBlue, DarkBlue, dll ada di sini
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CreateEventScreen(navController: NavController) {
+
+}
